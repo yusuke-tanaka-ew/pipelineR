7ac07bd5f5bd94c26bc606650e2ef725a4e6872a
yusuke-tanaka-ew <yusuke.tanaka.keio@gmail.com>
2021-02-19 19:43:53 +0900

    Add base line funciton   - model     - meta_mod_read     - meta_mod_add     - meta_mod_output_add   - output     - output_read

--- pipelineR/R/model.R
+++ pipelineR/R/model.R
@@ -10,3 +10,46 @@
 model_create <- function(model_name,processer_name){
     # TODO Create model script
 }
+
+
+#' Add meta data of model
+#'
+#' Add meta data of model to mod.yaml. If there is meta data of input model, function does nothing.
+#' @import usethis
+#' @param processer_name Target processer name.
+#' @param model_name Target model name.
+#' @export 
+#' @examples
+
+meta_mod_add <- function(model_name,processer_name){
+    ## TODO check mod.yaml & Add meta data to mod.yaml
+
+}
+
+
+
+#' Read meta data of model
+#'
+#' Read meta data of model to mod.yaml. If model_name is NULL (default is NULL), return all model meta data as list.
+#' @import usethis
+#' @param processer_name Target processer name.
+#' @param model_name Target model name.
+#' @export 
+#' @examples
+meta_mod_read <- function(model_name=NULL){
+    # TODO if model_name is null -> return all model meta data, if model_name is not null -> return meta data of model_name.
+}
+
+#' Add meta data of model's output.
+#'
+#' Add meta data of model's output to mod.yaml. If there is meta data of input model, or output, function does nothing.
+#' @import usethis
+#' @param model_name Target model name.
+#' @param output_name Target output name.
+#' @export 
+#' @examples
+
+meta_mod_output_add <- function(model_name,output_name){
+    ## TODO check mod.yaml & Add meta data to mod.yaml
+
+}
--- pipelineR/R/output.R
+++ pipelineR/R/output.R
@@ -1,11 +1,24 @@
-#' Controll output data (Save)
+#' Controll output data (Write)
 #'
-#' Controll output data. Save & Load output data by model name.
+#' Controll output data. Write output data by model name.
 #' @param model_name Target model name.
 #' @export 
 #' @examples
 
-output_write <- function(model_name){
+output_write <- function(x,model_name){
     ## TODO: Save output data @data/output/. File name is defined by model_name.
 
+}
+
+
+#' Controll output data ()
+#'
+#' Controll output data. Read output data by model name.
+#' @param model_name Target model name.
+#' @export 
+#' @examples
+
+output_read <- function(model_name){
+    ## TODO : Read output data ,Return list is contains meta data of output & output files.
+    
 }
